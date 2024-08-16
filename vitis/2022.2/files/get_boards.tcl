32875d4db706a68c7696df94198520f7cb01ab1c
Ben Hawks <bhawks@fnal.gov>
2024-08-16 11:35:55 -0500

    Add Alma8.1 and Vitis2022.2 dockerfiles

--- vitis/2022.2/files/get_boards.tcl
+++ vitis/2022.2/files/get_boards.tcl
@@ -1,7 +1,7 @@
 # Installs either all boards, or a specific board as specified in the $SPECIFIC_BOARD shell environment variable (empty string for all boards)
 set specific_board $::env(SPECIFIC_BOARD)
 
-xhub::refresh_catalog [xhub::get_xstores xilinx_board_store]
+vi
 
 if {$specific_board eq ""} {
     xhub::install [xhub::get_xitems]
--- vitis/2022.2/files/install.sh
+++ vitis/2022.2/files/install.sh
@@ -6,15 +6,15 @@ ${INSTALLER_PATH}/xsetup --agree XilinxEULA,3rdPartyEULA --batch Install --confi
 # Install Board files if desired
 # Set $SPECIFIC_BOARD if to install one board only, otherwise installs all availible boards
 # See https://github.com/Xilinx/XilinxBoardStore/wiki/Accessing-the-Board-Store-Repository
-	source /opt/Xilinx/Vitis/${VERSION}/settings64.sh
-	vitis -mode batch -source opt/get_boards.tcl
-	echo "Xilinx Board Store Install Complete (Vitis)"
+	source /opt/Xilinx/Vivado/${VERSION}/settings64.sh
+	vivado -mode batch -source opt/get_boards.tcl
+	echo "Xilinx Board Store Install Complete"
 	#Install Pynq-Z1 from github if requested (or all selected) because it's not availible on the board store... 
 	#if [$SPECIFIC_BOARD -eq "pynq-z1"] || [[ -z $SPECIFIC_BOARD ]]; then 
 		git clone https://github.com/cathalmccabe/pynq-z1_board_files.git /tmp/pynq-z1
-		mv /tmp/pynq-z1/pynq-z1 /opt/Xilinx/vitis/${VERSION}/data/boards/board_files/
+		mv /tmp/pynq-z1/pynq-z1 /opt/Xilinx/Vivado/${VERSION}/data/boards/board_files/
 		echo "Installed Pynq Boards:"
-		ls /opt/Xilinx/vitis/${VERSION}/data/boards/board_files/ | grep pynq 
+		ls /opt/Xilinx/Vivado/${VERSION}/data/boards/board_files/ | grep pynq 
 		echo "Pynq-Z1 Board Install complete"
 	#fi
 	echo "### Board Install Complete! ###"
